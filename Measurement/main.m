ac7928cf9a431490cab2fb2ffabc5dfc89531cce
Your Name <everettchurchey@crbc.edu>
2015-05-12 09:51:10 -0400

    Initial Commit

---
+++ Measurement/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Measurement
+//
+//  Created by Ej Churchey on 5/12/15.
+//  Copyright (c) 2015 Ej Churchey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
